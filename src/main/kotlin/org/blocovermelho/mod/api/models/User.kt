2b648301447c76458a9edad67868d7d6ddc65338
Alice Isabel <roridev@protonmail.com>
2023-11-25 20:22:11 -0300

    Use `DurationSerializer` on User's playtime

--- src/main/kotlin/org/blocovermelho/mod/api/models/User.kt
+++ src/main/kotlin/org/blocovermelho/mod/api/models/User.kt
@@ -1,6 +1,7 @@
 package org.blocovermelho.mod.api.models
 
 import kotlinx.serialization.Serializable
+import org.blocovermelho.mod.DurationSerializer
 
 import org.blocovermelho.mod.UUIDSerializer
 import java.util.*
@@ -14,7 +15,7 @@ data class User(
     val pronouns: List<String>,
     @Serializable(with = UUIDSerializer::class) val lastServer: UUID?,
     val lastPos: Map<@Serializable(with = UUIDSerializer::class) UUID, Pos>,
-    val playtime: Map<@Serializable(with = UUIDSerializer::class) UUID, Duration>
+    val playtime: Map<@Serializable(with = UUIDSerializer::class) UUID, @Serializable(with = DurationSerializer::class) Duration>
 )
 
 @Serializable
