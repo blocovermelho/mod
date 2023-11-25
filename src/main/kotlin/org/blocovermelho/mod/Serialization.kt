73bf7e04d15ce84867e510ba334311621fac2c42
Alice Isabel <roridev@protonmail.com>
2023-11-25 20:20:43 -0300

    Add custom serializer for rust's duration type
    It internally converts between Rust's std::time::Duration and Kotlin's
    kotlin.time.Duration

--- src/main/kotlin/org/blocovermelho/mod/Serialization.kt
+++ src/main/kotlin/org/blocovermelho/mod/Serialization.kt
@@ -1,11 +1,15 @@
 package org.blocovermelho.mod
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.util.*
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
 
 object UUIDSerializer : KSerializer<UUID> {
     override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
@@ -19,3 +23,19 @@ object UUIDSerializer : KSerializer<UUID> {
     }
 }
 
+object DurationSerializer: KSerializer<Duration> {
+    @Serializable
+    data class RustDuration(val secs: Long, val nanos: Long);
+
+    override val descriptor = PrimitiveSerialDescriptor("RUSTDURATION", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Duration {
+        val rust = RustDuration.serializer().deserialize(decoder);
+        return  rust.secs.seconds + rust.nanos.nanoseconds
+    }
+
+    override fun serialize(encoder: Encoder, value: Duration) {
+        val rust = RustDuration(value.inWholeSeconds, 0)
+        RustDuration.serializer().serialize(encoder, rust)
+    }
+}
