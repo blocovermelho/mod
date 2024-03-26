fb662e98e3f83fb1ffc02a7e5b66d67045dc347d
alikindsys <alice@blocovermelho.org>
2024-03-26 19:33:07 -0300

    [i11n] Migrate to localized strings

--- src/main/kotlin/org/blocovermelho/mod/commands/admin/AdminCommands.kt
+++ src/main/kotlin/org/blocovermelho/mod/commands/admin/AdminCommands.kt
@@ -2,9 +2,16 @@ package org.blocovermelho.mod.commands.admin
 
 import com.mojang.brigadier.CommandDispatcher
 import net.minecraft.server.command.ServerCommandSource
+import org.blocovermelho.mod.ext.isLogged
+import org.quiltmc.qkl.library.brigadier.register
 
 object AdminCommands {
     fun register(dispatcher: CommandDispatcher<ServerCommandSource>) {
-        Revoke.register(dispatcher)
+        dispatcher.register("bvadmin") {
+            //TODO: Luckperms integration
+            //Replace OP Check for luckperms api check.
+            requires { it.isPlayer && it.player!!.hasPermissionLevel(2) && it.player!!.isLogged() }
+            revokeCommand()
+        }
     }
 }
--- src/main/kotlin/org/blocovermelho/mod/commands/admin/Revoke.kt
+++ src/main/kotlin/org/blocovermelho/mod/commands/admin/Revoke.kt
@@ -1,146 +1,136 @@
 package org.blocovermelho.mod.commands.admin
 
-import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.context.CommandContext
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.network.ServerPlayerEntity
 import org.blocovermelho.mod.api.Routes
 import org.blocovermelho.mod.api.handleErr
 import org.blocovermelho.mod.ext.Helpers.command
-import org.blocovermelho.mod.ext.Helpers.maskedClipboard
+import org.blocovermelho.mod.ext.Helpers.translatableClipboard
 import org.blocovermelho.mod.ext.Other.serverHeader
+import org.blocovermelho.mod.ext.Rich.colorize
+import org.blocovermelho.mod.ext.Rich.lineOf
+import org.blocovermelho.mod.ext.Rich.lines
+import org.blocovermelho.mod.ext.Subcommand
 import org.blocovermelho.mod.ext.launch
 import org.blocovermelho.mod.ext.sendError
-import org.quiltmc.qkl.library.brigadier.argument.*
-import org.quiltmc.qkl.library.brigadier.register
+import org.blocovermelho.mod.service.NotificationService.playerNotFound
+import org.blocovermelho.mod.service.NotificationService.staffActor
+import org.quiltmc.qkl.library.brigadier.argument.enum
+import org.quiltmc.qkl.library.brigadier.argument.literal
+import org.quiltmc.qkl.library.brigadier.argument.player
+import org.quiltmc.qkl.library.brigadier.argument.value
 import org.quiltmc.qkl.library.brigadier.required
 import org.quiltmc.qkl.library.brigadier.util.player
 import org.quiltmc.qkl.library.brigadier.util.sendFeedback
 import org.quiltmc.qkl.library.text.Color
 import org.quiltmc.qkl.library.text.buildText
-import org.quiltmc.qkl.library.text.color
-import org.quiltmc.qkl.library.text.literal
+import org.quiltmc.qkl.library.text.translatable
 
+fun Subcommand.revokeCommand() {
+    required(literal("revoke")) { _ ->
+        required(enum("type", listOf("LINK", "ACCOUNT"))) { kind ->
+            required(player("user")) { user ->
+                launch {
+                    val kindVal = kind().value()
+                    val target = user().value()
 
-object Revoke {
-    fun register(dispatcher: CommandDispatcher<ServerCommandSource>) {
-        dispatcher.register("bvadmin") {
-            //TODO: Luckperms integration
-            //Replace OP Check for luckperms api check.
-            requires { it.isPlayer && it.player!!.hasPermissionLevel(2) }
-            required(literal("revoke")) { _ ->
-                required(enum("type", listOf("LINK", "ACCOUNT"))) { kind ->
-                    required(player("user")) { user ->
-                        launch {
-                            val kindVal = kind().value()
-                            val target = user().value()
-
-                            if ( kindVal.uppercase() == "LINK") {
-                                val account = Routes.User.Exists(target.uuid).handleErr { sendError(it, "verificando se o link existe") }
-                                    ?: return@launch
-
-                                if (!account) {
-                                    sendFeedback {
-                                        buildText {
-                                            serverHeader {
-                                                literal(" O jogador ")
-                                                color(Color.YELLOW) {
-                                                    literal(target.gameProfile.name)
-                                                }
-                                                literal(" não consta no sistema.")
-                                            }
-                                        }
-                                    }
-                                }
-
-                                val delete = Routes.User.Delete(target.uuid).handleErr { sendError(it, "apagando o link") }
-                                    ?: return@launch
-
-                                target.sendSystemMessage(buildText {
-                                    serverHeader {
-                                        literal(" O Link com o seu discord foi apagado com sucesso.\n")
-                                        literal(" Staff responsável: ")
-                                        color(Color.GREY) {
-                                            literal("${player!!.gameProfile.name}\n")
-                                        }
-
-                                        literal(" Clique na ")
-                                        maskedClipboard("<@${delete.discordId}>","menção da conta")
-                                        literal(" para verificar qual era a conta que estava linkado à sua previamente.\n")
-                                    }
-
-                                    serverHeader {
-                                        literal(" Use ")
-                                        command("/link")
-                                        literal(" para linkar sua outra conta.")
-                                    }
-                                })
-
-                                sendFeedback {
-                                    buildText {
-                                        serverHeader {
-                                            literal(" Link de ")
-                                            color(Color.YELLOW) {
-                                                literal(target.gameProfile.name)
-                                            }
-                                            literal(" apagado com sucesso.")
-                                        }
-                                    }
-                                }
-
-                            } else if ( kindVal.uppercase() == "ACCOUNT") {
-                                val account = Routes.Auth.Exists(target.uuid).handleErr { sendError(it, "verificando se a conta existe") }
-                                    ?: return@launch
-
-
-                                if (!account) {
-                                    sendFeedback {
-                                        buildText {
-                                            serverHeader {
-                                                literal(" O jogador ")
-                                                color(Color.YELLOW) {
-                                                    literal(target.gameProfile.name)
-                                                }
-                                                literal(" não consta no sistema.")
-                                            }
-                                        }
-                                    }
-                                }
-
-                                Routes.Auth.Delete(target.uuid).handleErr { sendError(it, "apagando a conta") }
-                                    ?: return@launch
-
-                                target.sendSystemMessage(buildText {
-                                    serverHeader {
-                                        literal(" A sua conta foi apagada com sucesso.\n")
-                                        literal(" Staff responsável: ")
-                                        color(Color.GREY) {
-                                            literal("${player!!.gameProfile.name}\n")
-                                        }
-                                    }
-
-                                    serverHeader {
-                                        literal(" Use ")
-                                        command("/registrar")
-                                        literal(" para criar uma nova senha para sua conta.")
-                                    }
-                                })
-
-                                sendFeedback {
-                                    buildText {
-                                        serverHeader {
-                                            literal(" Conta de ")
-                                            color(Color.YELLOW) {
-                                                literal(target.gameProfile.name)
-                                            }
-                                            literal(" apagado com sucesso.")
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                    if (kindVal.uppercase() == "LINK") {
+                        revokeLink(target)
+                    } else if ( kindVal.uppercase() == "ACCOUNT") {
+                        revokeAccount(target)
                     }
                 }
             }
+        }
+    }
+}
+suspend fun CommandContext<ServerCommandSource>.revokeLink(target: ServerPlayerEntity) {
+    val account = Routes.User.Exists(target.uuid).handleErr { sendError(it, "verificando se o link existe") }
+        ?: return
 
+    if (!account) {
+        sendFeedback {
+            buildText {
+                serverHeader {
+                    playerNotFound(target)
+                }
+            }
+        }
+    }
+
+    val delete = Routes.User.Delete(target.uuid).handleErr { sendError(it, "apagando o link") }
+        ?: return
+
+    target.sendSystemMessage(buildText {
+        serverHeader {
+            lines(
+                { translatable("bv.unlink.self.success") },
+                { staffActor(target) },
+                {
+                    lineOf(
+                        { translatable("bv.action.clipboard") },
+                        { translatableClipboard("<@${delete.discordId}>","bv.mask.discord_id") },
+                        { translatable("bv.unlink.see_previous") }
+                    )
+                }
+            )
+        }
+
+        serverHeader {
+            lineOf(
+                { translatable("bv.action.use", buildText { command("/link") }) },
+                { translatable("bv.link.hint")}
+            )
+        }
+    })
+
+    sendFeedback {
+        buildText {
+            serverHeader {
+                translatable("bv.unlink.of.success", colorize(target.gameProfile.name, Color.YELLOW))
+            }
+        }
+    }
+}
+
+suspend fun CommandContext<ServerCommandSource>.revokeAccount(target: ServerPlayerEntity) {
+    val account = Routes.Auth.Exists(target.uuid).handleErr { sendError(it, "verificando se a conta existe") }
+        ?: return
+
+
+    if (!account) {
+        sendFeedback {
+            buildText {
+                playerNotFound(target)
+            }
+        }
+    }
+
+    Routes.Auth.Delete(target.uuid).handleErr { sendError(it, "apagando a conta") }
+        ?: return
+
+    target.sendSystemMessage(buildText {
+        serverHeader {
+            lines(
+                { translatable("bv.revoke.self.success") },
+                { staffActor(player!!) }
+            )
+        }
+
+        serverHeader {
+            lineOf(
+                { translatable("bv.action.use", buildText { command("/registrar") }) },
+                { translatable("bv.register.hint") }
+            )
+        }
+    })
+
+    sendFeedback {
+        buildText {
+            serverHeader {
+                translatable("bv.revoke.of.success", colorize(target.gameProfile.name, Color.YELLOW))
+            }
         }
     }
 }
