a07545a16a4e8749e5165ed487be93e84790a8f8
alikindsys <alice@blocovermelho.org>
2024-03-26 23:57:49 -0300

    Add rule for automatically banning people who try to replace a carpet bot.

--- src/main/kotlin/org/blocovermelho/mod/events/AttemptLoginEvent.kt
+++ src/main/kotlin/org/blocovermelho/mod/events/AttemptLoginEvent.kt
@@ -11,14 +11,34 @@ import org.blocovermelho.mod.api.models.Ban
 import org.blocovermelho.mod.api.models.Cidr
 import org.blocovermelho.mod.ext.Mimicry.BanHammer.banMessage
 import org.blocovermelho.mod.ext.Other.newIdentity
+import org.blocovermelho.mod.ext.isCarpetBot
 import org.blocovermelho.mod.service.NotificationService
 import org.quiltmc.qkl.library.text.buildText
 import java.net.SocketAddress
+import java.util.*
 
 suspend fun onLoginAttempt(server: MinecraftServer, address: SocketAddress, profile: GameProfile) : Text? {
     BVQuilt.LOGGER.info("[PreLoginEvent] Processing ${address.address} for ${profile.id}")
     var text: Text? = null;
     val playerManager = server.playerManager;
+
+    val onlinePlayer = server.playerManager.getPlayer(profile.id)
+    val isCarpet = onlinePlayer?.isCarpetBot() ?: false;
+    val isOnline = onlinePlayer != null;
+    val isLoggedIn = BVQuilt.Store.LoggedPlayers.contains(profile.id);
+
+    val banReason = if (BVQuilt.Store.BanReasons.isEmpty()) { "Falsidade Ideológica" } else { BVQuilt.Store.BanReasons.random() }
+
+    if (isCarpet) {
+        BVQuilt.LOGGER.info("[PreLoginEvent] Tried replacing a Carpet Bot")
+        // This is a manual ban, issued to a random uuid. You'd need to actually ask for forgiveness
+        // for this to be removed since I said so. It is a ban which was issued automatically, but
+        // it will be needed to be removed manually. Trying to fuck up the bots as a joke is stupid.
+        Routes.Auth.CIDR.Ban(onlinePlayer!!.uuid, address.address, UUID.randomUUID())
+        NotificationService.newBan(banReason, playerManager, profile, address)
+        // There won't ever be an account for a carpet bot.
+        return buildText { banMessage(banReason, profile.name) }
+    }
 
     val user = Routes.User.Exists(profile.id).handleErr {  } ?: return Text.of("Internal API Error");
 
@@ -31,10 +51,6 @@ suspend fun onLoginAttempt(server: MinecraftServer, address: SocketAddress, prof
         BVQuilt.LOGGER.info("[PreLoginEvent] ${profile.id} is bypassing the CIDR check")
         return null
     }
-
-    val isOnline = server.playerManager.getPlayer(profile.id) != null;
-    val isLoggedIn = BVQuilt.Store.LoggedPlayers.contains(profile.id);
-    val banReason = if (BVQuilt.Store.BanReasons.isEmpty()) { "Falsidade Ideológica" } else { BVQuilt.Store.BanReasons.random() }
 
     if (isOnline && isLoggedIn) {
         BVQuilt.LOGGER.info("[PreLoginEvent] ${profile.id} is already online and logged in.")
