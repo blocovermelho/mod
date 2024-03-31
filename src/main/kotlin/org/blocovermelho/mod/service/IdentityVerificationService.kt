887be5d51b37ed523b08879679ba4feda8bd8058
alikindsys <alice@blocovermelho.org>
2024-03-31 15:39:33 -0300

    Remove early return on consumeEach

--- src/main/kotlin/org/blocovermelho/mod/service/IdentityVerificationService.kt
+++ src/main/kotlin/org/blocovermelho/mod/service/IdentityVerificationService.kt
@@ -18,17 +18,21 @@ object IdentityVerificationService {
             val spe = server.playerManager.getPlayer(it.uuid);
 
             if (spe != null && spe.isBypassing()) {
-                val nonce = BVQuilt.Store.BypassCidrCheck[spe.uuid] ?: return@consumeEach;
-                val response = Routes.Auth.CIDR.Allow(spe.uuid, spe.ip, nonce).handleErr { err -> spe.sendErr(err, "Verificando sua identidade") } ?: return@consumeEach
-                if (response) {
-                    BVQuilt.Store.BypassCidrCheck.remove(it.uuid)
-                    BVQuilt.LOGGER.info("[IdentityVerificationService] Verified ${it.uuid}.")
-                    NotificationService.successfulIdentity(spe)
+                val nonce = BVQuilt.Store.BypassCidrCheck[spe.uuid];
+                if (nonce != null) {
+                    val response = Routes.Auth.CIDR.Allow(spe.uuid, spe.ip, nonce).handleErr { err -> spe.sendErr(err, "Verificando sua identidade") } ?: return@consumeEach
+                    if (response) {
+                        BVQuilt.Store.BypassCidrCheck.remove(it.uuid)
+                        BVQuilt.LOGGER.info("[IdentityVerificationService] Verified ${it.uuid}.")
+                        NotificationService.successfulIdentity(spe)
+                    } else {
+                        BVQuilt.LOGGER.warn("[IdentityVerificationService] Failed Verifying ${it.uuid}.")
+                        NotificationService.failedIdentity(spe)
+                    }
+                    spe.updateCommandTree()
                 } else {
-                    BVQuilt.LOGGER.warn("[IdentityVerificationService] Failed Verifying ${it.uuid}.")
-                    NotificationService.failedIdentity(spe)
+                    BVQuilt.LOGGER.error("[IdentityVerificationService] UUID ${it.uuid} had null nonce.")
                 }
-                spe.updateCommandTree()
             }
         }
         BVQuilt.LOGGER.error("[IdentityVerificationService] Closed.")
